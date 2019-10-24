142607868c2d54372c450b7cb3a3e6592a1f10d5
israelqwe <israelcet@hotmail.com>
2019-10-24 14:18:41 -0500

    20/15/19
    modifica el archivo

--- app/src/main/java/com/androiddesdecero/seguridadencriptarandroid/MainActivity.java
+++ app/src/main/java/com/androiddesdecero/seguridadencriptarandroid/MainActivity.java
@@ -1,15 +1,21 @@
 package com.androiddesdecero.seguridadencriptarandroid;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 
@@ -27,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tv;
     Button button;
     Intent myfile;
+    Uri file2cipher=null;//archivo a cifrar
 
     //String apiKeyEncriptada ="0SPrEK0JntQ2qCm9cPEabw==";
     //String passwordEncriptacion = "gdsawr";
@@ -124,13 +131,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         //super.onActivityResult(requestCode, resultCode, data);
+        Uri uri=null;
         switch (requestCode){
             case 10:
                 if (resultCode==RESULT_OK){
+                    uri=data.getData();
                     String path=data.getData().getPath();
                     tv.setText(path);
+                    file2cipher=uri;
+                    Log.d("uri",uri.toString());
+                    //alterDocument(uri);
                 }
                 break;
+        }
+    }
+    private void alterDocument(Uri uri) {
+        try {
+            ParcelFileDescriptor pfd = this.getContentResolver().
+                    openFileDescriptor(uri, "rw");
+            Log.d("file d",pfd.toString());
+            FileOutputStream fileOutputStream =
+                    new FileOutputStream(pfd.getFileDescriptor());
+            fileOutputStream.write(("Overwritten by MyCloud at " +
+                    System.currentTimeMillis() + "\n").getBytes());
+            // Let the document provider know you're done by closing the stream.
+            fileOutputStream.close();
+            pfd.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 /*
