4891bb6dcc95f3a19f6e14d82eb7f2017ed5eee6
israelqwe <israelcet@hotmail.com>
2019-10-24 15:13:08 -0500

    cifra
    cifra y des cifra archivo

--- app/src/main/java/com/androiddesdecero/seguridadencriptarandroid/MainActivity.java
+++ app/src/main/java/com/androiddesdecero/seguridadencriptarandroid/MainActivity.java
@@ -13,9 +13,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 
@@ -83,12 +86,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         btEncriptar = findViewById(R.id.mainActivityBtEncriptar);
+        ///cifrarrrr
         btEncriptar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
-                    textoSalida = encriptar(etTexto.getText().toString(), key);
-                    tvTexto.setText(textoSalida);
+                    textoSalida = cifrarF(readSavedDataR(file2cipher), key);
+                    alterDocument(file2cipher,textoSalida.getBytes());
+                    //tvTexto.setText(textoSalida);
                 } catch (Exception e){
                     e.printStackTrace();
                 }
@@ -96,19 +101,21 @@ public class MainActivity extends AppCompatActivity {
         });
 //00000000000000009d
         btDesEncriptar = findViewById(R.id.mainActivityBtDesencritar);
+        ////des cifrar
         btDesEncriptar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try{
-                    textoSalida = desencriptar(textoSalida, key);
-                    tvTexto.setText(textoSalida);
+                    textoSalida = descifrar(readSavedDataR(file2cipher), key);
+                    //tvTexto.setText(textoSalida);
+                    alterDocument(file2cipher,textoSalida.getBytes());
                 }catch (Exception e){
                     e.printStackTrace();
                 }
             }
         });
     }
-
+/*
     private String desencriptar(String datos, SecretKey key) throws Exception{
         //SecretKeySpec secretKey = generateKey(password);
         Cipher cipher = Cipher.getInstance("AES");
@@ -117,8 +124,17 @@ public class MainActivity extends AppCompatActivity {
         byte[] datosDesencriptadosByte = cipher.doFinal(datosDescoficados);
         String datosDesencriptadosString = new String(datosDesencriptadosByte);
         return datosDesencriptadosString;
+    }*/
+    private String descifrar(String datos, SecretKey key) throws Exception{
+        //SecretKeySpec secretKey = generateKey(password);
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] datosDescoficados = Base64.decode(datos, Base64.DEFAULT);
+        byte[] datosDesencriptadosByte = cipher.doFinal(datosDescoficados);
+        String datosDesencriptadosString = new String(datosDesencriptadosByte);
+        return datosDesencriptadosString;
     }
-
+/*
     private String encriptar(String datos, SecretKey key) throws Exception{
         //SecretKeySpec secretKey = generateKey(password);
         Cipher cipher = Cipher.getInstance("AES");
@@ -127,7 +143,17 @@ public class MainActivity extends AppCompatActivity {
         String datosEncriptadosString = Base64.encodeToString(datosEncriptadosBytes, Base64.DEFAULT);
         return datosEncriptadosString;
         ////hola
+    }*/
+    private String cifrarF(String datos, SecretKey key) throws Exception{
+        //SecretKeySpec secretKey = generateKey(password);
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] datosEncriptadosBytes = cipher.doFinal(datos.getBytes());
+        String datosEncriptadosString = Base64.encodeToString(datosEncriptadosBytes, Base64.DEFAULT);
+        return datosEncriptadosString;
+        ////hola
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         //super.onActivityResult(requestCode, resultCode, data);
@@ -145,16 +171,23 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
-    private void alterDocument(Uri uri) {
+    private void alterDocument(Uri uri,byte[] bytes) {
         try {
             ParcelFileDescriptor pfd = this.getContentResolver().
-                    openFileDescriptor(uri, "rw");
-            Log.d("file d",pfd.toString());
+                    openFileDescriptor(uri, "w");
+           FileInputStream fis=new FileInputStream(pfd.getFileDescriptor());
+            String tex=readSavedData(pfd);
+            //int i=fis.read();
+            Log.d("file d", "("+tex+")");
             FileOutputStream fileOutputStream =
                     new FileOutputStream(pfd.getFileDescriptor());
+
+            fileOutputStream.write(bytes);
+            /*
             fileOutputStream.write(("Overwritten by MyCloud at " +
-                    System.currentTimeMillis() + "\n").getBytes());
+                    System.currentTimeMillis() + "\n").getBytes());*/
             // Let the document provider know you're done by closing the stream.
+            fileOutputStream.flush();
             fileOutputStream.close();
             pfd.close();
         } catch (FileNotFoundException e) {
@@ -162,6 +195,46 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    public String readSavedData ( ParcelFileDescriptor p) {
+        StringBuffer datax = new StringBuffer("");
+        try {
+            FileInputStream fIn =new  FileInputStream(p.getFileDescriptor()) ;
+            InputStreamReader isr = new InputStreamReader ( fIn ) ;
+            BufferedReader buffreader = new BufferedReader ( isr ) ;
+
+            String readString = buffreader.readLine ( ) ;
+            while ( readString != null ) {
+                datax.append(readString);
+                readString = buffreader.readLine ( ) ;
+            }
+
+            isr.close ( ) ;
+        } catch ( IOException ioe ) {
+            ioe.printStackTrace ( ) ;
+        }
+        return datax.toString() ;
+    }
+    public String readSavedDataR ( Uri uri) throws FileNotFoundException {
+        StringBuffer datax = new StringBuffer("");
+        ParcelFileDescriptor pfd = this.getContentResolver().
+                openFileDescriptor(uri, "r");
+        try {
+            FileInputStream fIn =new  FileInputStream(pfd.getFileDescriptor()) ;
+            InputStreamReader isr = new InputStreamReader ( fIn ) ;
+            BufferedReader buffreader = new BufferedReader ( isr ) ;
+
+            String readString = buffreader.readLine ( ) ;
+            while ( readString != null ) {
+                datax.append(readString);
+                readString = buffreader.readLine ( ) ;
+            }
+
+            isr.close ( ) ;
+        } catch ( IOException ioe ) {
+            ioe.printStackTrace ( ) ;
+        }
+        return datax.toString() ;
     }
 /*
     private SecretKeySpec generateKey(String password) throws Exception{
